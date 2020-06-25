e63a09f0ead2f6d2871c147d963067ce7987a683
Cat <ccallcottstevens@gmail.com>
2020-06-26 09:36:25 +1000

    created components foler and Todos file

--- src/App.js
+++ src/App.js
@@ -1,11 +1,26 @@
 import React, { Component } from 'react';
+import Todos from './components/Todos';
+
+
 import './App.css';
 
 class App extends Component {
+state = {
+  todos: [
+    {
+      id: 1,
+      title: 'Take out the rubbish',
+      completed: false
+    }
+  ]
+}
+
+
+
   render() {
     return (
       <div className="App">
-       <h1>Hello App</h1>
+       <Todos />
       </div>
     );
   }
