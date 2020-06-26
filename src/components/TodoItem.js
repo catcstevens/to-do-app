c89b7113890b90becb766eae049a7f84cb54e799
Cat <ccallcottstevens@gmail.com>
2020-06-26 19:20:14 +1000

    added delete buttons to todos and add items input bar

--- src/components/TodoItem.js
+++ src/components/TodoItem.js
@@ -2,10 +2,25 @@ import React, { Component } from 'react'
 import PropTypes from 'prop-types';
 
 export class TodoItem extends Component {
+    getStyle = () => {
+        return {
+            background: 'f4f4f4',
+            padding: '10px',
+            borderBottom: '1px #ccc dotted',
+            textDecoration: this.props.todo.completed ? 'line-through' : 'none'
+        }    
+}
+
     render() {
+        const { id, title } = this.props.todo;
         return (
-            <div style={itemStyle}>
-               <h3>{ this.props.todo.title }</h3> 
+            <div style={this.getStyle()}>
+            <p> 
+             <input type="checkbox" onChange={this.props.markComplete.bind
+             (this, id)} /> {' '}
+                { title }
+                <button onClick={this.props.delTodo.bind(this, id)} style={btnStyle}>x</button>
+            </p>
             </div>
         )
     }
@@ -14,11 +29,15 @@ export class TodoItem extends Component {
 TodoItem.propTypes = {
     todo: PropTypes.object.isRequired
 }
+const btnStyle = {
+    background: '#ff0000',
+    color: '#fff',
+    padding: '5px 8px',
+    borderRadius: '50%',
+    cursor: 'pointer',
+    float: 'right'
 
-const itemStyle = {
-    backgroundColor: '#f4f4f4'
 }
-
 
 
 export default TodoItem
